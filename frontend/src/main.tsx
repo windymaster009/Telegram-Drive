71fadabac016b44782c3eed7a58de96a37814b36
WinDY <71076716+windymaster009@users.noreply.github.com>
2026-09-05 01:13:15 +0700

    Route public share links without login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ShareLinkHost } from "./components/dashboard/ShareLinkHost";
+import { PublicSharePage } from "./components/share/PublicSharePage";
 import "./mobile-polish.css";
+
+const params = new URLSearchParams(window.location.search);
+const queryShareToken = params.get("share");
+const pathMatch = window.location.pathname.match(/^\/share\/([^/]+)\/?$/);
+const pathShareToken = pathMatch ? decodeURIComponent(pathMatch[1]) : null;
+const shareToken = queryShareToken || pathShareToken;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {shareToken ? (
+      <PublicSharePage token={shareToken} />
+    ) : (
+      <>
+        <App />
+        <ShareLinkHost />
+      </>
+    )}
   </React.StrictMode>,
 );
